c355dd378862f7c47811e928da05ce3dcbc7ec97
gogakoreli <gogakoreli@gmail.com>
2018-11-03 23:11:13 +0400

    implement get input stream observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { getInputStream } from './input';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular7-snake-game';
+  private unsubscribe$ = new Subject<boolean>();
+
+  constructor() {
+    getInputStream()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(input => {
+        console.log(input);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
