6f48e9ca66752421849e25f6a39de1233c1bc9d6
gogakoreli <gogakoreli@gmail.com>
2018-11-04 18:10:23 +0400

    add initial models for game logic

---
+++ src/app/models.ts
@@ -0,0 +1,24 @@
+export interface SnakePart {
+  i: number;
+  j: number;
+}
+
+export interface Snake {
+  head: SnakePart;
+  parts: SnakePart[];
+  direction: Direction;
+  length: number;
+}
+
+export enum Direction {
+  None = -1,
+  North = 0,
+  East = 1,
+  South = 2,
+  West = 3,
+}
+
+export interface Food {
+  i: number;
+  j: number;
+}
